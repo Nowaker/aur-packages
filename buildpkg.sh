7a6bc238c84239335ab4f45e7ffa9348b16c52db
Nowaker <damian.nowak@geozone.pl>
2012-12-06 03:31:38 +0100

    CH-26 własne paczki Javy - oraclejdk7-32, oraclejdk7-64 i generator repozytorium

---
+++ buildpkg.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+root=`pwd`
+repo="`pwd`/repo"
+mkdir -p "$repo"
+pkgext=""
+
+for pkg in "$@"; do
+  dir="$root/$pkg"
+  [ -d "$dir" ] || continue
+  cd "$dir"
+
+  makepkg -c && {    
+    mv *.tar "$repo"
+    find . -name PKGBUILD -prune -o -exec rm {} \;
+  }
+done
+
+repo-add -f "$repo/shur.db.tar.xz" "$repo"/*.tar
