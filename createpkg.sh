83ac19be94745227a0a15ea1f101335e73db3256
Nowaker <damian.nowak@geozone.pl>
2013-01-13 16:49:03 +0100

    skrypt do tworzenia nowej paczki

---
+++ createpkg.sh
@@ -0,0 +1,17 @@
+#/usr/bin/env zsh
+
+pkgname="$1"
+
+if [ -z "$pkgname" ]; then
+  echo "Usage: $0 package-name"
+  exit 1
+fi
+
+if [ -d "$pkgname" ]; then
+  echo "Directory $pkgname/ already exists."
+  exit 2
+fi
+
+mkdir -p "$pkgname"
+cp PKGBUILD.example "$pkgname" "$pkgname/PKGBUILD"
+cp example.install "$pkgname/$pkgname.install"
